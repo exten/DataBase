d8306ee2fc2126507822254d300698865187dc83
exten <exten@126.com>
2013-09-16 22:26:00 +0800

    e22.25

--- src/zero/mor/test/CharCode.java
+++ src/zero/mor/test/CharCode.java
@@ -5,7 +5,7 @@ import java.util.Random;
 import org.hibernate.CustomEntityDirtinessStrategy;
 
 import zero.hibernate.entity.Customes;
-//exten 2009
+//exten 2009.0.0
 public class CharCode
 {
 	public static void main(String[] args)
